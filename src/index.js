a270cfa116d215429f6e2963c203ed69a1bd3020
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-09-07 18:08:30 -0400

    add hot loader to index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,23 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+
+	 <App/>
+	, document.getElementById('root'));
 registerServiceWorker();
+
+// Hot Module Replacement API
+
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default
+    ReactDOM.render(
+      <NextApp />,
+      document.getElementById('root'));
+
+
+  })
+}
