ea909bc3b2d86a46ad6f94556cd69bc85bddda3a
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-09-07 22:12:30 -0400

    Add player component and styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import {HashRouter} from 'react-router-dom';
 import Header from './component/Header';
-import './App.css';
+import Player from './container/Player';
+import './style/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header/>
-      </div>
+      <div className="components-container">
+                <Header/>
+                <HashRouter>
+                    <Player/>
+                </HashRouter>
+        </div>
     );
   }
 }
